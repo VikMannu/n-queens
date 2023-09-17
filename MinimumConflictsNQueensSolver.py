b4f084f5c2b494bcf4fecf8992918f9a7365da6f
Victor Ayala <victor.ayala2a@gmail.com>
2023-09-17 03:36:07 -0400

    Second option and for me it is the best.

--- MinimumConflictsNQueensSolver.py
+++ MinimumConflictsNQueensSolver.py
@@ -1,72 +1,70 @@
 import random
 import time
+from collections import Counter
 
 
 class MinimumConflictsNQueensSolver:
-    def __init__(self, N, max_time=30):
+    def __init__(self, N, max_attempts=10, max_time=2):
+        self.conflicts = None
         self.N = N
-        self.board = [-1] * N
-        self.max_attempts = N + 1
+        self.board = list(range(self.N))
+        self.max_attempts = max_attempts
         self.max_time = max_time
 
-    def is_valid(self, row, col):
+    def calculate_conflicts_reparations(self):
+        conflicts_reparations = []
+        for row in range(self.N):
+            if not self.is_valid_queen(row, self.board[row]):
+                for col in range(self.N):
+                    if col != self.board[row] and self.is_valid_queen(row, col):
+                        conflicts_reparations.append((row, col))
+
+        if not conflicts_reparations:
+            return None  # No conflicts to repair
+
+        counter_conflicts_reparations = Counter(key for key, _ in conflicts_reparations)
+        queen_with_less_conflict = min(counter_conflicts_reparations, key=counter_conflicts_reparations.get)
+        conflicts_reparations = list(filter(lambda x: x[0] == queen_with_less_conflict, conflicts_reparations))
+        return random.choice(conflicts_reparations)
+
+    def is_valid_queen(self, row, col):
         # Check if it's safe to place a queen at position (row, col)
         for i in range(row):
             if self.board[i] == col or abs(self.board[i] - col) == abs(i - row):
                 return False
         return True
 
-    def find_min_conflict(self, row):
-        # Find the column that minimizes the number of conflicts and satisfies diagonal constraints
-        conflicts = []
-        for col in range(self.N):
-            if self.is_valid(row, col):
-                conflicts.append(self.count_conflicts(row, col))
-            else:
-                conflicts.append(float('inf'))  # Mark as infinite conflict if not valid
-        min_conflicts = min(conflicts)
-        min_conflict_cols = [i for i, c in enumerate(conflicts) if c == min_conflicts]
-        return random.choice(min_conflict_cols)
+    def is_valid_board(self):
+        # Check if the board is valid (no conflicts)
+        for row in range(self.N):
+            for col in range(row + 1, self.N):
+                if (
+                        self.board[row] == self.board[col] or
+                        abs(self.board[row] - self.board[col]) == abs(row - col)
+                ):
+                    return False
+        return True
 
-    def count_conflicts(self, row, col):
-        # Count the number of queens that can attack the queen at position (row, col)
-        conflict = 0
-        for i in range(row):
-            if self.board[i] == col or abs(self.board[i] - col) == abs(i - row):
-                conflict += 1
-        return conflict
+    def random_board(self):
+        # Create a random initial board configuration with distinct values
+        random.shuffle(self.board)
 
     def solve(self):
-        row = 0
-        attempts = 0
-        start_time = time.time()
-
-        while row < self.N:
-            attempts += 1
-            col = self.find_min_conflict(row)
-
-            if self.is_valid(row, col):
+        while not self.is_valid_board():
+            self.random_board()
+            attempts = 0
+            start_time = time.time()
+            print("Initial Board")
+            self.print_board()
+            while attempts < self.max_attempts or time.time() - start_time <= self.max_time:
+                conflicts_reparations = self.calculate_conflicts_reparations()
+                if conflicts_reparations is None:
+                    return  # No conflicts to repair
+                row, col = conflicts_reparations
                 self.board[row] = col
-                row += 1
-            else:
-                # Backtrack if unable to place the queen in the current row
-                row -= 1
-
-            # Check if the maximum number of attempts has been reached
-            if attempts >= self.max_attempts:
-                self.board = [-1] * N
-                row = 0
-                attempts = 0
-                start_time = time.time()
-
-            # Check if the maximum time has been exceeded
-            if time.time() - start_time >= self.max_time:
-                self.board = [-1] * N
-                row = 0
-                attempts = 0
-                start_time = time.time()
-
-        return self.board
+                if self.is_valid_board():
+                    return
+                attempts += 1
 
     def print_board(self):
         if self.board is None:
@@ -78,7 +76,7 @@ class MinimumConflictsNQueensSolver:
 
 if __name__ == "__main__":
     N = 8  # Change N according to the desired board size
-    max_attempts = N  # Maximum number of attempts before resetting
-    solver = MinimumConflictsNQueensSolver(N, max_attempts)
-    solution_board = solver.solve()
+    solver = MinimumConflictsNQueensSolver(N)
+    solver.solve()
+    print("Solved Board")
     solver.print_board()
